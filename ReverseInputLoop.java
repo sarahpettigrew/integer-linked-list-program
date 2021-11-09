60cbc744d2434e489865de61173cca2b9609bd0f
sarahpettigrew <sarahepettigrew@outlook.com>
2021-11-09 22:32:58 +0000

    commented main file, created a while loop file for continuous user input, created a file that removes words with peculiar punctuation

---
+++ ReverseInputLoop.java
@@ -0,0 +1,43 @@
+/**
+ * Write a description of class NumMath here.
+ *
+ * @author Sarah Pettigrew
+ * @version 10/10/2021
+ */
+import java.util.Scanner;
+import java.util.Stack;
+public class ReverseInputLoop
+{     
+    public static void main(String[] args) {
+        String regex = "[^a-zA-Z0-9\\-']|^-|-$";
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter words to reverse: (Enter \"STOP\" to exit) ");  
+        String userInput = input.nextLine();
+
+        while(!userInput.toUpperCase().equals("STOP")){
+            Stack<String> inputStack = new Stack<String>();
+       
+            String[] userWords = userInput.split("\\s+");
+            String reversedUserWords = "";
+            
+            for(String word:userWords){
+                word = word.replaceAll(regex, "");
+                if(!word.isEmpty()){
+                    inputStack.push(word);
+                }  
+            }
+            
+            while(!inputStack.isEmpty()){
+                String singleWord = inputStack.pop();
+                reversedUserWords += singleWord + " ";
+            }
+    
+            System.out.println("Reversed: " + reversedUserWords.trim() + "\n----------");
+            System.out.print("Enter words to reverse: (Enter \"STOP\" to exit) ");  
+            userInput = input.nextLine();
+        }
+        
+        input.close();   
+    }
+}
